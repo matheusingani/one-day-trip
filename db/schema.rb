7a4ec5b56b17a0559061835c8f11ebfcf404e25f
Renato Schneeberger <renato.schneeberger@iam-lab.ch>
2024-02-27 15:01:12 +0000

    implemented logout functionality

--- db/schema.rb
+++ db/schema.rb
@@ -14,16 +14,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_125428) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "expericences", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "place_id", null: false
-    t.integer "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["place_id"], name: "index_expericences_on_place_id"
-    t.index ["user_id"], name: "index_expericences_on_user_id"
-  end
-
   create_table "experiences", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "place_id", null: false
@@ -57,8 +47,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_125428) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "expericences", "places"
-  add_foreign_key "expericences", "users"
   add_foreign_key "experiences", "places"
   add_foreign_key "experiences", "users"
   add_foreign_key "places", "users"
